165e49a7ffcc7710ce1e9b4e6a0bce73717881c0
HARSH KUMAWAT <115946158+harshk04@users.noreply.github.com>
2026-06-03 23:40:33 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -113,6 +113,22 @@ export default function RootLayout({
             }),
           }}
         />
+        <script
+          type="text/javascript"
+          dangerouslySetInnerHTML={{
+            __html: `
+              var Tawk_API = Tawk_API || {}, Tawk_LoadStart = new Date();
+              (function() {
+                var s1 = document.createElement("script"), s0 = document.getElementsByTagName("script")[0];
+                s1.async = true;
+                s1.src = 'https://embed.tawk.to/6a206c016d77da1c401dc899/1jq7abqml';
+                s1.charset = 'UTF-8';
+                s1.setAttribute('crossorigin', '*');
+                s0.parentNode.insertBefore(s1, s0);
+              })();
+            `,
+          }}
+        />
       </body>
     </html>
   );
